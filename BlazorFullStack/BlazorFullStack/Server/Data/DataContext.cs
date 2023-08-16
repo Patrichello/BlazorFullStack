be6b5fc5fd559bc5af8a4b46b0fb54c6b8a6702a
Patrichello <iaroslavbeliakov@gmail.com>
2023-08-16 00:45:48 -0700

    Last Update

---
+++ BlazorFullStack/BlazorFullStack/Server/Data/DataContext.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
+
+namespace BlazorFullStack.Server.Data
+{
+	public class DataContext : DbContext
+	{
+        private readonly string _connectionString;
+
+        public DataContext(DbContextOptions<DataContext> options, IConfiguration configuration) : base(options)
+		{
+            _connectionString = configuration.GetConnectionString("DefaultConnection");
+        }
+     
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Comic>().HasData(
+                 new Comic { Id = 1, Name = "Marvel" },
+                 new Comic { Id = 2, Name = "DC" }
+            );
+
+            modelBuilder.Entity<SuperHero>().HasData(
+                new SuperHero
+            {
+                Id = 1,
+                FirstName = "Peter",
+                LastName = "Parker",
+                HeroName = "Spiderman",
+                ComicId = 1,
+            },
+
+            new SuperHero
+            {
+                Id = 2,
+                FirstName = "Bruce",
+                LastName = "Wayne",
+                HeroName = "Batman",
+                ComicId = 2,
+            }
+         
+            );
+        }
+
+        public DbSet<SuperHero> SuperHeroes { get; set; }
+
+        public DbSet<Comic> Comics { get; set; }
+
+        
+    }
+}
+
---
+++ BlazorFullStack/BlazorFullStack/Shared/Comic.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace BlazorFullStack.Shared
+{
+	public class Comic
+	{
+		public int Id { get; set; }
+
+		public string Name { get; set; } = string.Empty;
+	}
+}
+
